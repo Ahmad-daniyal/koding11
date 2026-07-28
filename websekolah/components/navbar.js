0a1b6a743028dee97dcdc2d3d7697d27cc4b133b
Ahmad-daniyal <daniyalhamim@gmail.com>
2026-07-28 10:35:34 +0700

    navbar

--- websekolah/components/navbar.js
+++ websekolah/components/navbar.js
@@ -5,7 +5,7 @@ const active = this.props.activePage;
 const linkClass = (page) => (page === active ? "active" : "");
 return `
 <nav>
-<h2>SMA Negeri Harapan Bangsa</h2>
+<h2>SMK Yadika konoha</h2>
 <a class="${linkClass('home')}" href="#/home">Home</a> |
 <a class="${linkClass('about')}" href="#/about">About</a> |
 <a class="${linkClass('kontak')}" href="#/kontak">Kontak</a>
