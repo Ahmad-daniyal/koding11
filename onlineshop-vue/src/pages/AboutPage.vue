40076f0293f7296605ca6c75a9342d728778c2c5
Ahmad-daniyal <daniyalhamim@gmail.com>
2026-08-03 09:12:29 +0700

    lier

--- onlineshop-vue/src/pages/AboutPage.vue
+++ onlineshop-vue/src/pages/AboutPage.vue
@@ -4,6 +4,8 @@
 <template>
   <main>
     <h1>About Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Minima sequi fugit vel quis facilis, nisi veritatis voluptas fuga! Enim asperiores incidunt necessitatibus repellendus commodi fuga! Fugiat officia ad cumque molestiae!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br> Pariatur, dolore rerum minus provident aut illum asperiores quos labore ab, modi sapiente voluptas porro odio culpa! Consectetur suscipit dolor obcaecati possimus?</p>
   </main>
 </template>
 
